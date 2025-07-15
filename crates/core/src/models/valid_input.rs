10a8e04fa9d36e7f343f4125d60a3b9d63fdf2bb
Alexander Cyon <alex.cyon@gmail.com>
2025-07-15 10:43:05 +0200

    fix display

--- crates/core/src/models/valid_input.rs
+++ crates/core/src/models/valid_input.rs
@@ -5,7 +5,7 @@ use crate::prelude::*;
 /// the items to be invoiced, the layout of the invoice, and an optional output path
 /// for the generated PDF file.
 #[derive(Debug, Clone, Display, Builder, Getters)]
-#[display("Layout: {}, Month: {}, out: {:?}, items: {}, language: {}", layout, period, maybe_output_path.as_ref().map(|d|d.display()), items, language)]
+#[display("Layout: {}, Period: {}, out: {:?}, items: {}, language: {}", layout, period, maybe_output_path.as_ref().map(|d|d.display()), items, language)]
 pub struct ValidInput {
     /// The language to use for the invoice, used on labels, headers etc.
     /// Defaults to English (`Language::EN`).
