eeef8018229ac14060fa7778fd03350efe7d63af
Alexander Cyon <Sajjon@users.noreply.github.com>
2026-02-21 21:47:46 +0100

    Fix include str for foundation typ (#35)

--- crates/foundation/src/lib.rs
+++ crates/foundation/src/lib.rs
@@ -4,4 +4,4 @@ mod typst_layouts;
 
 pub use crate::models::{AbstractNamedPdf, FontIdentifier, FontWeight, Pdf, save_pdf};
 pub use crate::traits::{FontRequiring, ToTypst, ToTypstFn};
-pub use crate::typst_layouts::TYPST_LAYOUT_TEST;
+pub use crate::typst_layouts::{TYPST_LAYOUT_FOUNDATION, TYPST_LAYOUT_TEST};
